40eba4fc3941159d79a45e8daece8299b7ce1580
lin brick <linbrick@lindeMacBook-Pro.local>
2020-12-06 22:46:40 +0800

    开发button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
+import Button, { ButtonSize, ButtonType } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button disabled> Add </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Add </Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled> Baidu Link </Button>
     </div>
   );
 }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 
 export enum ButtonSize {
   Large = 'lg',
@@ -17,5 +18,36 @@ interface BaseButtonProps {
   disabled?: boolean;
   size?: ButtonSize;
   btnType?: ButtonType;
-  children: React.ReactNode
+  children: React.ReactNode,
+  href?: string;
 }
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+  const {
+    disabled,
+    size,
+    btnType,
+    children,
+    href
+  } = props
+  const classes = classNames('btn', {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
+  })
+  if(btnType === ButtonType.Link && href) {
+    return (
+      <a className={classes} href={href}>{ children }</a>
+    )
+  }
+  return (
+    <button className={classes} disabled={disabled}>{ children }</button>
+  )
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button
