e56dba5687223a8bb5bb44d846541a97ee20c33c
lin brick <527563266@qq.com>
2021-06-01 23:11:26 +0800

    开发menu组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
@@ -7,6 +9,18 @@ function App() {
       <Button onClick={()=>{alert('add')}}> Add </Button>
       <Button btnType="primary" size="lg"> Add </Button>
       <Button btnType="danger" href="https://www.baidu.com" disabled> Baidu Link </Button>
+
+      <Menu>
+        <MenuItem>
+          cool link 1
+        </MenuItem>
+        <MenuItem>
+          cool link 2
+        </MenuItem>
+        <MenuItem>
+          cool link 3
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
